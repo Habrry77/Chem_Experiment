872dde4afe7e9d3aa9ff6273401f36f1b1d75099
Habrry <habrry77@outlook.com>
2026-03-14 22:16:06 +0800

    Auto

--- push.sh
+++ push.sh
@@ -1,5 +1,5 @@
 git remote add origin https://github.com/Habrry77/Chem_Experiment.git
 git add .
 git commit -m "Auto"
-git pull origin master
+git pull origin master --aloow-unrelated-histories
 git push origin master
